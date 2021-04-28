9af32bb336c9214431c353a97e5b1c7bf2a9fe68
chase-abbott <cabbott93@gmail.com>
2021-04-28 15:23:02 -0700

    wire up search

--- src/Search.js
+++ src/Search.js
@@ -3,23 +3,44 @@ import './Search.css';
 
 export default class Search extends Component {
   state = {
-
+    nameFilter: '',
+    sortField: '',
   }
 
-  handleSubmit = ({ target }) => {
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
 
+  handleNameFilter = ({ target }) => {
+    this.setState({ nameFilter: target.value });
+  }
+
+  handleSortField = ({ target }) => {
+    this.setState({ sortField: target.value });
   }
 
   render() {
+    const { nameFilter, sortField } = this.state;
     return (
+
       <form 
         className="Search"
         onSubmit={this.handleSubmit}>
-        <input></input>
-        <select>
+
+        <input
+          name="nameFilter"
+          value={nameFilter}
+          onChange={this.handleNameFilter}
+        ></input>
+
+        <select
+          name="sortField"
+          value={sortField}
+          onChange={this.handleSortField}
+        >
           <option value="">...sort by</option>
           <option value="type"> Type </option>
-          <option value="secondary"> Secondary Type</option>
+          <option value="secondary-type"> Secondary Type</option>
           <option value="attack"> Attack </option>
           <option value="height"> Height </option>
         </select>
