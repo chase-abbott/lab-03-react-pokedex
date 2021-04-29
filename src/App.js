74206f964a2fa90c8d0dec37b407fc8e04866761
chase-abbott <cabbott93@gmail.com>
2021-04-29 13:32:41 -0700

    add call backs to page forward and back

--- src/App.js
+++ src/App.js
@@ -29,9 +29,11 @@ class App extends Component {
   }
 
   async getPokemon() {
+    const { page } = this.state;
 
     const response = await request
-      .get(POKEMON_API_URL);
+      .get(POKEMON_API_URL)
+      .query({ page : page });
 
     const primaryTypes = [...new Set(response.body.results.map(p => p.type_1))];
     const secondaryTypes = [...new Set(response.body.results.map(p => p.type_2))];
@@ -48,7 +50,7 @@ class App extends Component {
       sortParameter, 
       type_1Sort, 
       type_2Sort,
-      page } = this.state;
+    } = this.state;
 
     const response = await request
       .get(POKEMON_API_URL)
@@ -57,10 +59,19 @@ class App extends Component {
         type_2: type_2Sort, 
         direction : sortDirection, 
         pokemon : search,
-        page: page });
+      });
 
     this.setState({ pokemon : response.body.results });
   }
+  
+  async pageChange() {
+    const { page } = this.state;
+    const response = await request
+      .get(POKEMON_API_URL)
+      .query({ page : page });
+
+    this.setState({ pokemon: response.body.results });
+  } 
 
   handleSearch = ({ search, sortField, typeSort, secondaryTypeSort, direction }) => {
     this.setState(
@@ -68,54 +79,59 @@ class App extends Component {
         sortParameter : sortField, 
         type_1Sort : typeSort, 
         type_2Sort : secondaryTypeSort,
-        sortDirection : direction },
+        sortDirection : direction,
+      },
       () => this.sortPokemon(),
     );
-
-
   }
+
 
   handlePageForward = () => {
     const { page } = this.state;
-    this.setState({ page: (page + 1) });
+    this.setState({ page: (page + 1) },
+      () => this.pageChange()
+    );
+    
   }
 
-   handlePageBack = () => {
-     const { page } = this.state;
-     if (page > 1){
-       this.setState({ page: (page - 1) });
-     }
-   }
-
-    handlePageChange = ({ target }) => {
-      this.setState({ page: target.value });
-    }
-    
-    render() {
-      const { pokemon, primaryTypes, secondaryTypes, page } = this.state;
-      
-      return (
-        <div className="App">
-          <Header/>
-          <Search 
-            primaryTypes={primaryTypes} 
-            secondaryTypes={secondaryTypes}
-            onSearch={this.handleSearch}
-            page={page}
-            pageForward={this.handlePageForward}
-            pageBack={this.handlePageBack}
-            pageChange={this.handPageChange}
-          />
-          <main>
-            {pokemon && (pokemon.length
-              ? <PokeList pokemon={pokemon}/> 
-              : <p> Sorry! That doesn't exist</p>
-            )}
-          </main>
-          <Footer/>
-        </div>
+  handlePageBack = () => {
+    const { page } = this.state;
+    if (page > 1){
+      this.setState({ page: (page - 1) },
+        () => this.pageChange()
       );
     }
+  }
+
+  // handlePageChange = ({ target }) => {
+  //   this.setState({ page: target.value });
+  // }
+    
+  render() {
+    const { pokemon, primaryTypes, secondaryTypes, page } = this.state;
+      
+    return (
+      <div className="App">
+        <Header/>
+        <Search 
+          primaryTypes={primaryTypes} 
+          secondaryTypes={secondaryTypes}
+          onSearch={this.handleSearch}
+          page={page}
+          pageForward={this.handlePageForward}
+          pageBack={this.handlePageBack}
+          pageChange={this.handlePageChange}
+        />
+        <main>
+          {pokemon && (pokemon.length
+            ? <PokeList pokemon={pokemon}/> 
+            : <p> Sorry! That doesn't exist</p>
+          )}
+        </main>
+        <Footer/>
+      </div>
+    );
+  }
 
 }
 
