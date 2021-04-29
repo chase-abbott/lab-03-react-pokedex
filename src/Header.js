06290ab115d5e2c786c736f31719d48913d3fdc4
chase-abbott <cabbott93@gmail.com>
2021-04-29 07:15:20 -0700

    add pokeball image

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
   render() {
     return (
       <header className="Header">
-        <img src="./pokeball.png" alt="pokeball"/>
+        <img src="pokeball.png" alt="pokeball"/>
         <h1> Chase's Pokedex </h1>
       </header>
     );
