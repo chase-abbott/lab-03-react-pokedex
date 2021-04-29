decd05deceb96093e28dc4b4ddb3d7b6e3b594fd
chase-abbott <cabbott93@gmail.com>
2021-04-29 14:45:29 -0700

    add items per page dropdown

--- src/Footer.js
+++ src/Footer.js
@@ -9,6 +9,15 @@ export default class Footer extends Component {
         <h6>
             Gotta catch 'em all
         </h6>
+        <label>
+          Items per page
+          <select>
+            <option value="10"> 10 </option>
+            <option value="25"> 25 </option>
+            <option value="50"> 50</option>
+            <option value="100"> 100</option>
+          </select>
+        </label>
       </footer>
     );
   }
