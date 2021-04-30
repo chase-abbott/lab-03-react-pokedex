6fc9eaf661cecf05af5a0c82bd82019b4468647d
chase-abbott <cabbott93@gmail.com>
2021-04-30 16:17:37 -0700

    add pokedex links to item rather than link in item

--- src/PokeItem.js
+++ src/PokeItem.js
@@ -6,22 +6,22 @@ export default class PokeItem extends Component {
   render() {
     const { monster } = this.props;
     return (
-      <li className="PokeItem" 
-        style= {{
-          backgroundColor: `${monster.color_1}`
-        }}
-          
+      <a href={monster.pokedex} target="_blank" rel="noreferrer">
+        <li className="PokeItem" 
+          style= {{
+            backgroundColor: `${monster.color_1}`
+          }}
+          href={monster.pokedex}
+        >
+          <h2> {monster.pokemon} </h2>
+          <img src={monster.url_image} alt={monster.pokemon}></img>
+          <h4> Type: {monster.type_1}</h4>
+          <h4> Secondary Type: {monster.type_2}</h4>
+          <h4> Attack: {monster.attack}</h4>
+          <h4> Height: {monster.height}</h4>
         
-        
-      >
-        <h2> {monster.pokemon} </h2>
-        <img src={monster.url_image} alt={monster.pokemon}></img>
-        <h4> Type: {monster.type_1}</h4>
-        <h4> Secondary Type: {monster.type_2}</h4>
-        <h4> Attack: {monster.attack}</h4>
-        <h4> Height: {monster.height}</h4>
-        <a href={monster.pokedex} target="_blank" rel="noreferrer"> PokeDex </a>
-      </li>
+        </li>
+      </a>
     );
   }
 
